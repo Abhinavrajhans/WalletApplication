73ffabf74cf9b0a096dcf41ef740469ed66b317e
abhinavrajhans <abhinav.rajhans1@gmail.com>
2025-11-11 20:49:18 +0530

    first commit

---
+++ src/main/java/com/example/ShardedSagaWallet/ShardedSagaWalletApplication.java
@@ -0,0 +1,24 @@
+package com.example.ShardedSagaWallet;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class ShardedSagaWalletApplication {
+
+
+	public static void main(String[] args) {
+		Dotenv dotenv=Dotenv.configure().load();
+
+		//Set environment variables
+		dotenv.entries().forEach((entry)->{
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
+		SpringApplication.run(ShardedSagaWalletApplication.class, args);
+	}
+
+}
